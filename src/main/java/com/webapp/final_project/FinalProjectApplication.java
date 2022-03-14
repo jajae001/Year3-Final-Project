6990ae7c7d7f6a76951cdcaed01c7289731fd332
jajae001 <ajaelu4@gmx.co.uk>
2022-03-14 19:10:19 +0000

    file ordering sonar update

--- src/main/java/com/webapp/final_project/FinalProjectApplication.java
+++ src/main/java/com/webapp/final_project/FinalProjectApplication.java
@@ -2,10 +2,9 @@ package com.webapp.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan({"com.webapp.final_project","HomeContoller"})
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
