d1b2a647421f211d644e7d0cad3b899b2f55d6c3
jajae001 <ajaelu4@gmx.co.uk>
2022-04-20 14:45:28 +0100

    Updata controller

--- src/main/java/com/webapp/final_project/FinalProjectApplication.java
+++ src/main/java/com/webapp/final_project/FinalProjectApplication.java
@@ -2,8 +2,9 @@ package com.webapp.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan(basePackages = "com.webapp.final_project")
 @SpringBootApplication
 public class FinalProjectApplication {
 
